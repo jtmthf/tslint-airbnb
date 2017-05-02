9802eb9a2a10aef946f573413a83ca65af7770aa
Jack Moore <jtmthf@mst.edu>
2017-05-01 22:10:33 -0500

    Define project structure

---
+++ configs/base/best-practices.ts
@@ -0,0 +1,18 @@
+export const rules = {
+  // enforces getter/setter pairs in objects
+  // 'accessor-pairs': false,
+
+  // enforces return statements in callbacks of array's methods
+  // http://eslint.org/docs/rules/array-callback-return
+  // 'array-callback-return': true,
+
+  // treat var statements as if they were block scoped
+  // 'blocked-scoped-var': true,
+
+  // specify the maximum cyclomatic complexity allowed in a program
+  'cyclomatic-complexity': false,
+
+  // enforce that class methods use "this"
+  // http://eslint.org/docs/rules/class-methods-use-this
+  // 'class-methods-use-this': true,
+}
